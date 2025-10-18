f53c5a8c9c0acf77317a5ac686c2cf84ef991e42
Jon Cavallie Mester <jonmester3@gmail.com>
2025-10-19 01:21:43 +0200

    refactor and separate code

--- kv/kv.go
+++ kv/kv.go
@@ -30,12 +30,6 @@ type Store struct {
 	clock *Clock
 }
 
-type Txn struct {
-	s       *Store
-	startTS Timestamp
-	writes  map[string]string
-}
-
 func (s *Store) Begin() *Txn {
 	s.mu.Lock()
 	start := s.clock.Now()
@@ -46,33 +40,6 @@ func (s *Store) Begin() *Txn {
 		startTS: start,
 		writes:  make(map[string]string),
 	}
-}
-
-func (tx *Txn) Put(key, val string) {
-	tx.writes[key] = val
-}
-
-func (tx *Txn) Commit(ctx context.Context) error {
-	tx.s.mu.Lock()
-	defer tx.s.mu.Unlock()
-
-	for k := range tx.writes {
-		if vs := tx.s.versions[k]; len(vs) > 0 && vs[0].TS > tx.startTS {
-			return errors.New("write-write conflict - key updated after txn start")
-		}
-	}
-
-	commitTS := tx.s.clock.Now()
-
-	for k, v := range tx.writes {
-		tx.s.state[k] = v
-		tx.s.versions[k] = append([]Version{{TS: commitTS, Value: v}}, tx.s.versions[k]...)
-		tx.s.log = append(tx.s.log, Entry{Key: k, Value: v})
-	}
-
-	tx.s.commitIndex++
-	tx.s.signalAppliedLocked()
-	return nil
 }
 
 func New() *Store {
---
+++ kv/tsx.go
@@ -0,0 +1,39 @@
+package kv
+
+import (
+	"context"
+	"errors"
+)
+
+type Txn struct {
+	s       *Store
+	startTS Timestamp
+	writes  map[string]string
+}
+
+func (tx *Txn) Put(key, val string) {
+	tx.writes[key] = val
+}
+
+func (tx *Txn) Commit(ctx context.Context) error {
+	tx.s.mu.Lock()
+	defer tx.s.mu.Unlock()
+
+	for k := range tx.writes {
+		if vs := tx.s.versions[k]; len(vs) > 0 && vs[0].TS > tx.startTS {
+			return errors.New("write-write conflict - key updated after txn start")
+		}
+	}
+
+	commitTS := tx.s.clock.Now()
+
+	for k, v := range tx.writes {
+		tx.s.state[k] = v
+		tx.s.versions[k] = append([]Version{{TS: commitTS, Value: v}}, tx.s.versions[k]...)
+		tx.s.log = append(tx.s.log, Entry{Key: k, Value: v})
+	}
+
+	tx.s.commitIndex++
+	tx.s.signalAppliedLocked()
+	return nil
+}
